11545cab0e41dec04b48b91830d919306b5cb2a0
jmilkowski <jmilkowski@i2asolutions.com>
2020-12-30 23:49:52 +0100

    base templates login, logout views, base templates, model improvements

--- projects_and_tasks/admin.py
+++ projects_and_tasks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from projects_and_tasks.models import Project, Task, UserProfile
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
--- projects_and_tasks/tests.py
+++ projects_and_tasks/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from projects_and_tasks.models import Project, Task
+
+
+class TaskModelTest(TestCase):
+
+    def test_task_create_increment_project_task_counter(self):
+        project = Project.objects.create(
+            name='test',
+            code='ABC'
+        )
+        self.assertEqual(project.task_counter, 0)
+        task = Task.objects.create(
+            project=project,
+            name="test_task",
+        )
+        self.assertEqual(task.number, 0)
+        project.refresh_from_db()
+        self.assertEqual(project.task_counter, 1)
+
